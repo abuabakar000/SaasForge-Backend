132443c8cd1a946f61290e3fa9e753b7f6179c5a
abubakar000 <abubakarkhawaja74@gmail.com>
2026-03-05 01:46:14 +0500

    fix: add root index.js entry point for render

---
+++ index.js
@@ -0,0 +1,3 @@
+// This file acts as a proxy entry point for environments (like Render)
+// that default to running `node index.js` if the Start Command is not overridden.
+require('./src/index.js');
